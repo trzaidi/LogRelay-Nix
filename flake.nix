0438efbbc21ad877b4b62a84c8e1a43dd4c3f0ce
Tai Zaidi <trzaidi@users.noreply.github.com>
2026-05-15 18:21:25 -0400

    created flake file

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  description = "NixOS flake and service module for LogRelay on NVIDIA Jetson Orin";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: {};
+}
